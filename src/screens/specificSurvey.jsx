2d6066e90f62cb44104143f740806b06dedb14f2
Abdullah Bin Imran <abi220419@gmail.com>
2024-07-10 16:17:01 +0500

    Added Survey Modals and Final Submission Survey Page.

--- src/screens/specificSurvey.jsx
+++ src/screens/specificSurvey.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Image, TouchableOpacity, Alert, Modal, ScrollView } from "react-native";
+import { View, StyleSheet, Image, TouchableOpacity, Alert, Modal, ScrollView, Switch } from "react-native";
 import Text from "../components/text";
 import LinearGradient from "react-native-linear-gradient";
 import { launchImageLibrary } from 'react-native-image-picker';
 import { request, PERMISSIONS } from 'react-native-permissions';
+import { BlurView } from '@react-native-community/blur';
+
 
 const SpecificSurvey = ({ route }) => {
   const { title } = route.params;
   const [selectedImages, setSelectedImages] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isSubmitModalVisible, setIsSubmitModalVisible] = useState(false);
+  const [sendNowActive, setSendNowActive] = useState(false);
+  const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false); // State for success modal
+
+
 
   const requestPermissions = (permission) => {
     return request(permission).then(result => {
@@ -61,6 +68,19 @@ const SpecificSurvey = ({ route }) => {
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
   };
+
+  const toggleSubmitModal = () => {
+    setIsSubmitModalVisible(!isSubmitModalVisible);
+  };
+
+  const toggleSendNow = () => {
+    setSendNowActive(!sendNowActive);
+  };
+
+  const toggleSuccessModal = () => {
+    setIsSuccessModalVisible(!isSuccessModalVisible);
+  };
+
 
   return (
     <View style={styles.container}>
@@ -138,16 +158,17 @@ const SpecificSurvey = ({ route }) => {
           </TouchableOpacity>
         </View>
 
-      <TouchableOpacity
-        style={styles.nextButton}
-        onPress={() => {
-          // Navigate to the next screen
-        }}
-      >
+      <TouchableOpacity style={styles.nextButton} onPress={toggleSubmitModal}>
         <Text style={[styles.next, styles.typo]}>Next</Text>
       </TouchableOpacity>
 
       <Modal visible={isModalVisible} transparent={true} animationType="slide">
+        <BlurView
+          style={styles.blur}
+          blurType="light"
+          blurAmount={5}
+          reducedTransparencyFallbackColor="white"
+        />
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <ScrollView contentContainerStyle={styles.modalScrollView}>
@@ -170,6 +191,81 @@ const SpecificSurvey = ({ route }) => {
           </View>
         </View>
       </Modal>
+
+
+      {/* Success Modal */}
+      <Modal visible={isSuccessModalVisible} transparent={true} animationType="slide">
+        <BlurView
+          style={styles.blur}
+          blurType="light"
+          blurAmount={5}
+          reducedTransparencyFallbackColor="white"
+        />
+        <View style={styles.modalContainer}>
+          <View style={styles.innerModal}>
+            <Image
+              style={styles.tickImage}
+              source={require('../images/tick-1.png')}
+              resizeMode="cover"
+            />
+            <Text style={styles.successText}>Survey Submitted Successfully</Text>
+            <TouchableOpacity style={styles.closeButton} onPress={toggleSuccessModal}>
+              <Text style={styles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+    </Modal>
+
+
+      {/* Survey Modal */}
+        <Modal visible={isSubmitModalVisible} transparent={true} animationType="slide">
+          <BlurView
+            style={styles.blurBackground}
+            blurType="light"
+            blurAmount={2}
+            reducedTransparencyFallbackColor="white"
+          />
+            <View style={styles.bottomModal}>
+              <View style={styles.surveyParent}>
+                <Text style={styles.survey}>Survey</Text>
+                <TouchableOpacity onPress={toggleSubmitModal}>
+                  <Image
+                    style={styles.iconX}
+                    resizeMode="cover"
+                    source={require("../images/cross.png")}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.rectangle} />
+              <View style={styles.textGroup}>
+                <Text style={styles.text4}>Recurring Reload Copying 2 Survey Machine</Text>
+                <View style={styles.frameView}>
+                  <TouchableOpacity style={styles.sendNowParent} onPress={toggleSendNow}>
+                    <Text style={styles.sendNow}>Send Now</Text>
+                    <View style={styles.switchContainer}>
+                      <Switch
+                        trackColor={{ false: '#767577', true: '#81b0ff' }}
+                        thumbColor={sendNowActive ? '#007bff' : '#f4f3f4'}
+                        onValueChange={toggleSendNow}
+                        value={sendNowActive}
+                        style={styles.switch} 
+                        touchSoundDisabled={true}
+                      />
+                    </View>
+                  </TouchableOpacity>
+                  <Text style={[styles.text5, sendNowActive ? styles.textActive : styles.textInactive]}>
+                    {sendNowActive ? "Results will be sent now." : "You can send later."}
+                  </Text>
+                </View>
+              </View>
+              <TouchableOpacity style={styles.typebuttonType2secondary1} onPress={() => {
+                toggleSubmitModal(); // Close the submit modal
+                toggleSuccessModal(); // Show success modal
+              }}>
+                <Text style={styles.submitSurveyText}>Submit Survey</Text>
+              </TouchableOpacity>
+            </View>
+        </Modal>
     </View>
   );
 };
@@ -179,6 +275,11 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: "#F0F0F0",
+  },
+  blur: {
+    position: "absolute",
+    top: 0, left: 0, bottom: 0, right: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)'
   },
   card: {
     backgroundColor: "white",
@@ -334,12 +435,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "white",
   },
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
   modalContent: {
     width: "80%",
     backgroundColor: "white",
@@ -370,6 +465,123 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   closeModalButtonText: {
+    fontSize: 16,
+    color: "white",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerModal: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  tickImage: {
+    width: 50,
+    height: 50,
+    marginBottom: 20,
+  },
+  successText: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: 'black',
+  },
+  closeButton: {
+    backgroundColor: '#007bff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  // Styling for the 'Next' Button popup modal
+  blurBackground: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  bottomModal: {
+    width: "100%",
+    backgroundColor: "white",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+  },
+  surveyParent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  survey: {
+    fontSize: 24,
+    color: 'black',
+    fontFamily: "SourceSans3-Bold",
+  },
+  iconX: {
+    width: 25,
+    height: 25,
+  },
+  textGroup: {
+    marginBottom: 20,
+  },
+  text4: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: 'black',
+    fontFamily: 'Outfit-SemiBold',
+  },
+  frameView: {
+    flexDirection: "column",
+    marginTop: 10,
+  },
+  sendNowParent: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  sendNow: {
+    fontSize: 15,
+    color: 'black',
+    fontFamily: 'Outfit-SemiBold',
+  },
+  text5: {
+    fontSize: 13,
+    marginBottom: 10,
+  },
+  switchContainer: {
+    justifyContent: 'center', 
+  },
+  switch: {
+    transform: [{ scaleX: 1.5 }, { scaleY: 1.3 }], 
+  },
+  textActive: {
+    color: "#007bff", // blue color
+  },
+  textInactive: {
+    color: "#555", // grey color
+  },
+  rectangle: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "#ccc",
+    marginBottom: 20,
+  },
+  typebuttonType2secondary1: {
+    backgroundColor: "#007bff",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  submitSurveyText: {
     fontSize: 16,
     color: "white",
   },
